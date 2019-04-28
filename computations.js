af0f06370f5c028e1f35153676916cd4d1623db7
kyle.rb <kyleryanboyle@gmail.com>
2019-04-28 19:03:02 -0400

    Now properly generates partition when a single point is in the intersection of triangles

--- computations.js
+++ computations.js
@@ -1,69 +1,31 @@
-// Various functions for doing computations on points.
-// These functions are called from render.js.
 
-function p(x, y) { // create a point
-  return { x: x, y: y };
-}
+//// Various functions for doing computations on points.
+//// These functions are called from render.js.
 
-// get a set of random points
-function randomPoints(count, min, max) {
-  let points = [];
-  for (let i = 0; i < count; i++) {
-    points.push(p(randRange(min.x, max.x), randRange(min.y, max.y)));
-  }
-  return points;
-}
 
+
+  ///////////////////////////////////
+ //// Generic utility functions ////
+///////////////////////////////////
 function randRange(min, max) {
   return Math.random() * (max-min) + min;
 }
 
-// center of a set of points (not a member of the set)
-function centerOf(points) {
-  let sum = points.reduce((sum, point) => p(sum.x+point.x, sum.y+point.y), p(0, 0));
-  return p(sum.x/points.length, sum.y/points.length);
+// integer list of range [a, b)
+function listFrom(a, b) {
+  return Array(b-a).fill(0).map((_, i) => i+a);
 }
 
-// angle from a to b
-function angle(a, b) {
-  return Math.atan2(b.y - a.y, b.x - a.x);
-}
-// returns a function for comparing points based on angle from a given center point
-function angleCompareFunction(center) {
-  return function(a, b) {
-    let angleA = angle(center, a), angleB = angle(center, b);
-    if (angleA > angleB) return 1;
-    if (angleA < angleB) return -1;
-    return 0;
-  }
+// rotates a list by offset, so [1, 2, 3] rotated by 1 is [2, 3, 1]
+function rotate(list, offset) {
+  return list.map((_,i) => list[(i + offset + list.length) % list.length]);
 }
 
-// intersection of two line segments from four points
-function intersect(a, b, c, d) {
-  let center = centerOf([a, b, c, d]);
-
-  let i;
-  i = lineIntersection(a, slope(a,b), c, slope(c,d));
-  if (inRect(i, a, b) && inRect(i, c, d)) return i;
-
-  i = lineIntersection(a, slope(a,c), b, slope(b,d));
-  if (inRect(i, a, c) && inRect(i, b, d)) return i;
-
-  i = lineIntersection(a, slope(a,d), b, slope(b,c));
-  if (inRect(i, a, d) && inRect(i, b, c)) return i;
-
-  return null; // if there's no intersection at all
-}
-
-// intersection of two lines defined by point p with slope m, and point q with slope n
-function lineIntersect(p, m, q, n) {
-  let x = (q.y - p.y - m*q.x + n*p.x) / (m - n);
-  let y = m*(x - p.x) - p.y; // point slope form
-  return p(x, y);
-}
-
-function slope(p, q) {
-  return (p.y - q.y) / (p.x - q.x);
+// generic compare for sort function
+function compare(a, b) {
+  if (a > b) return 1;
+  if (a < b) return -1;
+  return 0;
 }
 
 // if i is within the bounds of the quadrilateral bounded by a, b
@@ -72,20 +34,150 @@ function inRect(i, a, b) {
   let top = Math.min(a.y, b.y), bottom = Math.max(a.y, b.y);
   return i.x > left && i.x < right && i.y > top && i.y < bottom;
 }
-// if i is withint the triangle formed by a, b, c
+// if i is within the triangle formed by a, b, c
 function inTriangle(i, a, b, c) {
   return orient(i, a, b) === orient(i, b, c) === orient(i, c, a);
 }
+
 // returns 1 for a ccw triangle, -1 for cw, and 0 for collinear
 function orient(a, b, c) {
-  let det = det([[a.x-c.x, a.y-c.y], [b.x-c.x, b.y-c.y]]);
+  let det = determinant([[a.x-c.x, a.y-c.y], [b.x-c.x, b.y-c.y]]);
   if (det > 0) return 1;
   if (det < 0) return -1;
   return 0;
 }
-function det(matrix) {
+function determinant(matrix) {
   if (matrix.length === 2) return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
   return matrix.reduce((sum, vec, i) =>
     sum + (i%2*-2 + 1) * vec[0] * det(matrix.filter((vec, j) => i!==j).map((vec) => vec.slice(1))),
   0);
+}
+
+
+
+
+
+  /////////////////////////////////////
+ //// Geometric utility functions ////
+/////////////////////////////////////
+
+// create a point
+function p(x, y) {
+  return { x: x, y: y };
+}
+
+// get count random points between min and max
+function randomPoints(count, min, max) {
+  let points = [];
+  for (let i = 0; i < count; i++) {
+    points.push(p(randRange(min.x, max.x), randRange(min.y, max.y)));
+  }
+  return points;
+}
+
+function slope(p, q) {
+  return (p.y - q.y) / (p.x - q.x);
+}
+
+function angle(a, b) { // from a to b
+  return Math.atan2(b.y - a.y, b.x - a.x);
+}
+
+// center of a set of points (not a member of the set)
+function centerOf(points) {
+  let sum = points.reduce((sum, point) => p(sum.x+point.x, sum.y+point.y), p(0, 0));
+  return p(sum.x/points.length, sum.y/points.length);
+}
+
+// given four points, return the two line semgents which intersect, and their intersection
+function segmentIntersect(a, b, c, d) {
+  let center = centerOf([a, b, c, d]);
+
+  let i;
+  i = lineIntersection(a, slope(a,b), c, slope(c,d));
+  if (inRect(i, a, b) && inRect(i, c, d)) return { intersection: i, segments: [[a,b], [c,d]] };
+
+  i = lineIntersection(a, slope(a,c), b, slope(b,d));
+  if (inRect(i, a, c) && inRect(i, b, d)) return { intersection: i, segments: [[a,c], [b,d]] };
+
+  i = lineIntersection(a, slope(a,d), b, slope(b,c));
+  if (inRect(i, a, d) && inRect(i, b, c)) return { intersection: i, segments: [[a,d], [b,c]] };
+
+  return null; // if there's no intersection at all
+}
+// intersection of two lines defined by point p with slope m, and point q with slope n
+function lineIntersect(p, m, q, n) {
+  let x = (q.y - p.y - m*q.x + n*p.x) / (m - n);
+  let y = m*(x - p.x) - p.y; // point slope form
+  return p(x, y);
+}
+
+
+
+
+
+  /////////////////////////////////////
+ //// Tverberg-specific functions ////
+/////////////////////////////////////
+
+// iterate through points, for each point p:
+//  - check whether all halfspaces with a boundary through p contain 1/3 of the points
+//  - do this by checking each line that goes through p and another point, on both sides
+//     - each side should have at least n/3+1 and less than 2n/3 (or around there?)
+// if one matches, exit and get r-1 triangles based on the matching point as a center
+// if none match, we need to form a center point using two crossing line segments
+// iterate through sets of four points and find an intersection p
+//  - if the intersection doesn't exist, move on
+//  - if it does, then check all halfspaces through p like before
+// when one is found, exit, and get r-2 triangles from the remaining points based on that center
+// but I'm not sure how to guarantee that we'll definitely find a valid center
+function tverbergPartition(points) {
+  let n = points.length;
+  let lowerLimit = (n-1) / 3; // at least a third of points other than i
+
+  for (let i = 0; i < n; i++) { // test each point i
+    let pointValid = true;
+    for (let j = 0; j < n; j++) { // along with another point j
+      if (j === i) continue;
+      // count the points which are on the left of the line ij:
+      let countOnLeft = points.filter((p) => orient(points[i], points[j], p) > 0).length;
+      let countOnRight = n - countOnLeft - 2; // total - left - (i and j)
+      // i isn't factored into the midpoint metric
+      // and assume j isn't on either side, since we're testing worst case of the line position
+
+      // check whether the halfspaces in either direction of ij have sufficient points
+      if (countOnLeft < lowerLimit || countOnRight < lowerLimit) {
+        pointValid = false;
+        break; // if they don't, then break; move on to the next i
+      }
+    }
+    if (pointValid) { // if a point has all large enough halfspaces
+      return {
+        lines: [],
+        triangles: trianglesForPoints(points, points[i], [i]), // center i, don't include i
+      };
+    }
+  }
+
+  // TODO: test all intersection points
+
+  return { // no triangles :(
+    lines: [],
+    triangles: [],
+  };
+}
+
+
+// splits n points into subsets of triangles, each one's points spread out by n/3
+// n is the count of points, minus the count of except (points not to include in any triangles)
+function trianglesForPoints(points, center, except) {
+  let indices = listFrom(0, points.length); // indices referencing each point
+  except.forEach((e) => indices.splice(e, 1)); // remove exceptions from indices
+  // sort remaining indices by angle from the center to the index's corresponding point:
+  indices.sort((iA, iB) => compare(angle(center, points[iA]), angle(center, points[iB])));
+
+  let triCount = Math.floor(indices.length / 3);
+  return listFrom(0, triCount).map((t) => indices.filter((referenceIndex, literalIndex) => {
+    return literalIndex % triCount === t; // take 3 points which are triCount apart
+  }).slice(0,3)); // limit to 3
 }
--- render.js
+++ render.js
@@ -1,99 +1,171 @@
 // This file contains functions for rendering to the page, as well as some logic.
 // It relies on computations.js for a bunch of math and stuff.
 
-const black = '#000000';
-const colors = makePalette();
 
-// Currently, this is where everything is happening (this function runs on page load).
-// Right now all I'm doing is getting r-1 triangles that should intersect, based on Birch's paper:
-// - Generate a set of random points.
-// - Get their center of mass, sort them by angle with regard to that center.
-// - Split them into r-1 triangles (the last subset would be a single point).
-// - Draw each of those triangles.
-// - Draw the points, and the center of mass.
 
-// The next step is to actually get full intersections working.
-// I think I can brute force it by trying each point as the individual one, then if none are found,
-//  go through each set of four points to find an intersection that fits.
+
+let dragging = null; // point currently being dragged
+let recalcRequired = true; // set when point is moved, unset after a calculation
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
-  const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
+  let canvas = document.getElementById('canvas');
 
-  let r = 10; // set to however many subsets you want
+  let r = 4; // set to however many subsets you want
   let n = 3*r - 2;
+  let points = randomPoints(n, p(100, 100), p(canvas.width - 100, canvas.height - 100));
+  
+  let drag = new Drag(canvas, points);
+  let draw = new Draw(canvas.getContext('2d'), points, canvas.width, canvas.height);
 
-  ctx.lineWidth = 6;
+  const black = '#000000';
+  const colors = makePalette();
 
-  let points = randomPoints(n, p(100, 100), p(1800, 1000)); // n points 
-  let center = centerOf(points);
-  points.sort(angleCompareFunction(center));
+  let lines = [], triangles = [];
 
-  let triCount = r - 1; // in a real case, this might be r-2
-  for (let t = 0; t < triCount; t++) {
-    drawTriangle(ctx, points.filter((p, i) => i%(r-1) === t), colors[t]);
-  }
-
-  points.forEach((point, i) => drawDot(ctx, point, black));
-  drawStar(ctx, center, 10, black);
+  setInterval(function() { // render function, called every 16ms
+    if (drag.recalcRequired) {
+      ({ lines, triangles } = tverbergPartition(points));
+      drag.recalcRequired = false;
+    }
+    draw.clear();
+    triangles.forEach((tri, t) => draw.triangle(tri, colors[t]));
+    lines.forEach((line) => draw.line(line, black));
+    points.forEach((point) => draw.dot(point, black));
+  }, 16);
 });
 
 
 
-function drawDot(ctx, point, color) {
-  ctx.fillStyle = color;
-  ctx.beginPath();
-  ctx.ellipse(point.x, point.y, 10, 10, 0, 0, 2*Math.PI);
-  ctx.fill();
-}
-function drawStar(ctx, center, size, color) {
-  ctx.strokeStyle = color;
-  ctx.lineWidth = 6;
-  ctx.beginPath();
-  let x = center.x, y = center.y;
-  const points = [
-    [x-size*.5, y+size*.8],  // lower right
-    [x+size*.75, y-size*.1], // upper left
-    [x-size*.75, y-size*.1], // upper right
-    [x+size*.5, y+size*.8],  // lower left
-  ];
-  ctx.moveTo(x, y-size*.65); // start at top
-  points.forEach((p) => ctx.lineTo(...p));
-  ctx.closePath();
-  ctx.stroke();
+//// This class allows a set of points to be dragged around within the bounds of a canvas.
+class Drag {
+  constructor(canvas, points) {
+    this.canvas = canvas;
+    this.points = points;
+    this.dragging = null;
+    this.recalcRequired = true;
+
+    this.canvas.addEventListener('mousedown', this.mouseDown);
+    this.canvas.addEventListener('mouseup', this.mouseUp);
+    // Note: It's important that the methods in the Drag class are defined using arrow functions.
+    // When they're passed as callbacks, their 'this' value must not be bound to the event's 'this'.
+    // Arrow functions prevent the 'this' value from binding.
+  }
+
+  getMousePos = (event) => {
+    let rect = this.canvas.getBoundingClientRect(); // absolute size of canvas
+    let scaleX = this.canvas.width / rect.width;    // relationship bitmap vs. element for X
+    let scaleY = this.canvas.height / rect.height;  // relationship bitmap vs. element for Y
+  
+    return {
+      x: (event.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
+      y: (event.clientY - rect.top) * scaleY     // been adjusted to be relative to element
+    }
+  }
+
+  mouseDown = (event) => {
+    let mouse = this.getMousePos(event);
+    for (let i = 0; i < this.points.length; i++) {
+      let dx = mouse.x - this.points[i].x,
+          dy = mouse.y - this.points[i].y;
+      let dist2 = dx * dx + dy * dy;
+      if (dist2 < 150) { // distance less than 10 -> distance squared less than 100, plus padding
+        this.dragging = this.points[i];
+        this.canvas.addEventListener('mousemove', this.mouseMove);
+        return;
+      }
+    }
+    this.dragging = null; // dragging nothing
+  }
+
+  mouseMove = (event) => {
+    let mouse = this.getMousePos(event);
+    this.dragging.x = mouse.x;
+    this.dragging.y = mouse.y;
+  }
+
+  mouseUp = (event) => {
+    this.dragging = null;
+    this.recalcRequired = true;
+    canvas.removeEventListener('mousemove', this.mouseMove);
+  }
 }
 
-function drawTriangle(ctx, points, color) {
-  strokeTriangle(ctx, points, color);
-  fillTriangle(ctx, points, color, 0.1);
-}
-function strokeTriangle(ctx, points, color) {
-  ctx.strokeStyle = color;
-  ctx.lineWidth = 6;
-  ctx.beginPath();
-  ctx.moveTo(points[0].x, points[0].y);
-  ctx.lineTo(points[1].x, points[1].y);
-  ctx.lineTo(points[2].x, points[2].y);
-  ctx.closePath();
-  ctx.stroke();
-}
-function fillTriangle(ctx, points, color, alpha) { // with translucent color
-  ctx.fillStyle = color;
-  ctx.globalAlpha = alpha;
-  ctx.beginPath();
-  ctx.moveTo(points[0].x, points[0].y);
-  ctx.lineTo(points[1].x, points[1].y);
-  ctx.lineTo(points[2].x, points[2].y);
-  ctx.closePath();
-  ctx.fill();
-  ctx.globalAlpha = 1;
-}
-
-function drawPalette(ctx) {
-  colors.forEach((color, i) => dot(ctx, 100 + 100*(i%10), 100 + 100*Math.floor(i/10), color));
-}
 
 
+//// This class allows various things to be drawn to a 2D context, using a set of points.
+class Draw {
+  constructor(context, points, width, height) {
+    this.ctx = context;
+    this.points = points;
+    this.width = width;
+    this.height = height;
+  }
 
+  clear() {
+    this.ctx.clearRect(0, 0, this.width, this.height);
+  }
+
+  dot(point, color) {
+    this.ctx.fillStyle = color;
+  
+    this.ctx.beginPath();
+    this.ctx.ellipse(point.x, point.y, 10, 10, 0, 0, 2*Math.PI);
+    this.ctx.fill();
+  }
+  star(center, size, color) {
+    this.ctx.strokeStyle = color;
+    this.ctx.lineWidth = 6;
+
+    const x = center.x, y = center.y;
+    const starPoints = [
+      [x - size*.5,  y + size*.8],  // lower right
+      [x + size*.75, y - size*.1], // upper left
+      [x - size*.75, y - size*.1], // upper right
+      [x + size*.5,  y + size*.8],  // lower left
+    ];
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(x, y-size*.65); // start at top
+    starPoints.forEach((p) => this.ctx.lineTo(...p));
+    this.ctx.closePath();
+    this.ctx.stroke();
+  }
+
+  line(lineRef, color) {
+    this.ctx.strokeStyle = color;
+    this.ctx.lineWidth = 6;
+  
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.points[lineRef[0]]);
+    this.ctx.lineTo(this.points[lineRef[1]]);
+    this.ctx.strokePath();
+  }
+
+  triangle(triangleRef, color) {
+    let triangle = triangleRef.map((p) => this.points[p]); // map reference points to real ones
+  
+    this.ctx.beginPath();
+    this.ctx.moveTo(triangle[0].x, triangle[0].y);
+    this.ctx.lineTo(triangle[1].x, triangle[1].y);
+    this.ctx.lineTo(triangle[2].x, triangle[2].y);
+    this.ctx.closePath();
+  
+    this.ctx.strokeStyle = color;
+    this.ctx.lineWidth = 6;
+    this.ctx.stroke();
+  
+    this.ctx.fillStyle = color;
+    this.ctx.globalAlpha = 0.1;
+    this.ctx.fill();
+    this.ctx.globalAlpha = 1;
+  }
+}
+
+
+
+//// Functions for generating color
 function makePalette() { // create a set of colors
   const saturation = 1;
   const lightnesses = [.5, .3, .8]; // 3 levels of lightness
